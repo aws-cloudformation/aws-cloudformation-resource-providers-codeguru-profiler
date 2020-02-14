9ef546c909d3ef32f83919b783c85ab2d3ee360b
Lambros Petrou <petroul@amazon.com>
2020-02-14 17:58:22 +0000

    Implement the List handler

--- aws-codeguruprofiler-profilinggroup/src/main/java/software/amazon/codeguruprofiler/profilinggroup/ListHandler.java
+++ aws-codeguruprofiler-profilinggroup/src/main/java/software/amazon/codeguruprofiler/profilinggroup/ListHandler.java
@@ -1,9 +1,16 @@
 package software.amazon.codeguruprofiler.profilinggroup;
 
+import software.amazon.awssdk.services.codeguruprofiler.CodeGuruProfilerClient;
+import software.amazon.awssdk.services.codeguruprofiler.model.InternalServerException;
+import software.amazon.awssdk.services.codeguruprofiler.model.ListProfilingGroupsRequest;
+import software.amazon.awssdk.services.codeguruprofiler.model.ListProfilingGroupsResponse;
+import software.amazon.awssdk.services.codeguruprofiler.model.ThrottlingException;
+import software.amazon.cloudformation.exceptions.CfnServiceInternalErrorException;
+import software.amazon.cloudformation.exceptions.CfnThrottlingException;
 import software.amazon.cloudformation.proxy.AmazonWebServicesClientProxy;
 import software.amazon.cloudformation.proxy.Logger;
-import software.amazon.cloudformation.proxy.ProgressEvent;
 import software.amazon.cloudformation.proxy.OperationStatus;
+import software.amazon.cloudformation.proxy.ProgressEvent;
 import software.amazon.cloudformation.proxy.ResourceHandlerRequest;
 
 import java.util.ArrayList;
@@ -20,11 +27,35 @@ public class ListHandler extends BaseHandler<CallbackContext> {
 
         final List<ResourceModel> models = new ArrayList<>();
 
-        // TODO : put your code here
+        try {
+            CodeGuruProfilerClient profilerClient = CodeGuruProfilerClient.create();
 
-        return ProgressEvent.<ResourceModel, CallbackContext>builder()
-            .resourceModels(models)
-            .status(OperationStatus.SUCCESS)
-            .build();
+            ListProfilingGroupsRequest listProfilingGroupsRequest = ListProfilingGroupsRequest.builder()
+                    .includeDescription(true)
+                    .maxResults(100)
+                    .nextToken(request.getNextToken())
+                    .build();
+
+            ListProfilingGroupsResponse response = proxy.injectCredentialsAndInvokeV2(listProfilingGroupsRequest, profilerClient::listProfilingGroups);
+
+            response.profilingGroups().forEach(pg ->
+                    models.add(ResourceModel.builder()
+                            .profilingGroupName(pg.name())
+                            .arn(pg.arn()).build())
+            );
+
+            logger.log(String.format("%d \"%s\" has been successfully listed for token %s!", models.size(), ResourceModel.TYPE_NAME, request.getNextToken()));
+
+            return ProgressEvent.<ResourceModel, CallbackContext>builder()
+                    .resourceModels(models)
+                    .nextToken(response.nextToken())
+                    .status(OperationStatus.SUCCESS)
+                    .build();
+
+        } catch (InternalServerException e) {
+            throw new CfnServiceInternalErrorException(e);
+        } catch (ThrottlingException e) {
+            throw new CfnThrottlingException(e);
+        }
     }
 }
--- aws-codeguruprofiler-profilinggroup/src/test/java/software/amazon/codeguruprofiler/profilinggroup/ListHandlerTest.java
+++ aws-codeguruprofiler-profilinggroup/src/test/java/software/amazon/codeguruprofiler/profilinggroup/ListHandlerTest.java
@@ -1,18 +1,31 @@
 package software.amazon.codeguruprofiler.profilinggroup;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import software.amazon.awssdk.services.codeguruprofiler.model.InternalServerException;
+import software.amazon.awssdk.services.codeguruprofiler.model.ListProfilingGroupsRequest;
+import software.amazon.awssdk.services.codeguruprofiler.model.ListProfilingGroupsResponse;
+import software.amazon.awssdk.services.codeguruprofiler.model.ProfilingGroupDescription;
+import software.amazon.awssdk.services.codeguruprofiler.model.ThrottlingException;
+import software.amazon.cloudformation.exceptions.CfnServiceInternalErrorException;
+import software.amazon.cloudformation.exceptions.CfnThrottlingException;
 import software.amazon.cloudformation.proxy.AmazonWebServicesClientProxy;
 import software.amazon.cloudformation.proxy.Logger;
 import software.amazon.cloudformation.proxy.OperationStatus;
 import software.amazon.cloudformation.proxy.ProgressEvent;
 import software.amazon.cloudformation.proxy.ResourceHandlerRequest;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
+import static software.amazon.codeguruprofiler.profilinggroup.RequestBuilder.makeValidRequest;
 
 @ExtendWith(MockitoExtension.class)
 public class ListHandlerTest {
@@ -30,25 +43,65 @@ public class ListHandlerTest {
     }
 
     @Test
-    public void handleRequest_SimpleSuccess() {
-        final ListHandler handler = new ListHandler();
+    public void testSuccessState() {
+        final ResourceHandlerRequest<ResourceModel> request = makeValidRequest();
+        final String arn = "arn:aws:codeguru-profiler:us-east-1:000000000000:profilingGroup/IronMan-Suite-34";
+        final ProfilingGroupDescription profilingGroupDescription = ProfilingGroupDescription.builder()
+                        .name("IronMan-Suite-34")
+                        .arn(arn)
+                        .build();
 
-        final ResourceModel model = ResourceModel.builder().build();
+        doReturn(ListProfilingGroupsResponse.builder()
+                .profilingGroups(profilingGroupDescription)
+                .nextToken("test-token")
+                .build())
+                .when(proxy).injectCredentialsAndInvokeV2(
+                        ArgumentMatchers.eq(ListProfilingGroupsRequest
+                                .builder()
+                                .nextToken(null)
+                                .maxResults(100)
+                                .includeDescription(true)
+                                .build()),
+                        any());
 
-        final ResourceHandlerRequest<ResourceModel> request = ResourceHandlerRequest.<ResourceModel>builder()
-            .desiredResourceState(model)
-            .build();
+        final ProgressEvent<ResourceModel, CallbackContext> response
+                = new ListHandler().handleRequest(proxy, request, null, logger);
 
-        final ProgressEvent<ResourceModel, CallbackContext> response =
-            handler.handleRequest(proxy, request, null, logger);
+        final ResourceModel expectedModel = ResourceModel.builder()
+                .profilingGroupName(profilingGroupDescription.name())
+                .arn(profilingGroupDescription.arn())
+                .build();
 
         assertThat(response).isNotNull();
         assertThat(response.getStatus()).isEqualTo(OperationStatus.SUCCESS);
         assertThat(response.getCallbackContext()).isNull();
         assertThat(response.getCallbackDelaySeconds()).isEqualTo(0);
         assertThat(response.getResourceModel()).isNull();
-        assertThat(response.getResourceModels()).isNotNull();
+        assertThat(response.getResourceModels()).containsExactly(expectedModel);
+        assertThat(response.getNextToken()).isEqualTo("test-token");
         assertThat(response.getMessage()).isNull();
         assertThat(response.getErrorCode()).isNull();
+    }
+
+    @Test
+    public void testInternalServerException() {
+        final ResourceHandlerRequest<ResourceModel> request = makeValidRequest();
+
+        doThrow(InternalServerException.builder().build())
+                .when(proxy).injectCredentialsAndInvokeV2(any(), any());
+
+        assertThrows(CfnServiceInternalErrorException.class, () ->
+                new ListHandler().handleRequest(proxy, request, null, logger));
+    }
+
+    @Test
+    public void testThrottlingException() {
+        final ResourceHandlerRequest<ResourceModel> request = makeValidRequest();
+
+        doThrow(ThrottlingException.builder().build())
+                .when(proxy).injectCredentialsAndInvokeV2(any(), any());
+
+        assertThrows(CfnThrottlingException.class, () ->
+                new ListHandler().handleRequest(proxy, request, null, logger));
     }
 }

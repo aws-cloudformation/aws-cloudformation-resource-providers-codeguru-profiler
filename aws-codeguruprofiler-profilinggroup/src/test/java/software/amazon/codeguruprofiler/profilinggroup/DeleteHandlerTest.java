8477175ce853dcf678eeb8bd3f8a83f84c20c3cc
Lambros Petrou <petroul@amazon.com>
2020-02-14 12:31:02 +0000

    Implement the Delete handler

--- aws-codeguruprofiler-profilinggroup/src/test/java/software/amazon/codeguruprofiler/profilinggroup/DeleteHandlerTest.java
+++ aws-codeguruprofiler-profilinggroup/src/test/java/software/amazon/codeguruprofiler/profilinggroup/DeleteHandlerTest.java
@@ -1,17 +1,30 @@
 package software.amazon.codeguruprofiler.profilinggroup;
 
-import software.amazon.cloudformation.proxy.AmazonWebServicesClientProxy;
-import software.amazon.cloudformation.proxy.Logger;
-import software.amazon.cloudformation.proxy.OperationStatus;
-import software.amazon.cloudformation.proxy.ProgressEvent;
-import software.amazon.cloudformation.proxy.ResourceHandlerRequest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import software.amazon.awssdk.services.codeguruprofiler.model.DeleteProfilingGroupResponse;
+import software.amazon.awssdk.services.codeguruprofiler.model.InternalServerException;
+import software.amazon.awssdk.services.codeguruprofiler.model.ResourceNotFoundException;
+import software.amazon.awssdk.services.codeguruprofiler.model.ThrottlingException;
+import software.amazon.awssdk.services.codeguruprofiler.model.ValidationException;
+import software.amazon.cloudformation.exceptions.CfnInvalidRequestException;
+import software.amazon.cloudformation.exceptions.CfnNotFoundException;
+import software.amazon.cloudformation.exceptions.CfnServiceInternalErrorException;
+import software.amazon.cloudformation.exceptions.CfnThrottlingException;
+import software.amazon.cloudformation.proxy.AmazonWebServicesClientProxy;
+import software.amazon.cloudformation.proxy.Logger;
+import software.amazon.cloudformation.proxy.OperationStatus;
+import software.amazon.cloudformation.proxy.ProgressEvent;
+import software.amazon.cloudformation.proxy.ResourceHandlerRequest;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 
 @ExtendWith(MockitoExtension.class)
@@ -29,26 +42,79 @@ public class DeleteHandlerTest {
         logger = mock(Logger.class);
     }
 
-    @Test
-    public void handleRequest_SimpleSuccess() {
-        final DeleteHandler handler = new DeleteHandler();
-
-        final ResourceModel model = ResourceModel.builder().build();
-
-        final ResourceHandlerRequest<ResourceModel> request = ResourceHandlerRequest.<ResourceModel>builder()
-            .desiredResourceState(model)
+    private final ResourceModel validResourceModel = ResourceModel.builder()
+            .profilingGroupName("IronMan-Suite-34")
             .build();
 
+    private final ResourceModel invalidResourceModel = ResourceModel.builder()
+            .build();
+
+    @Test
+    public void testSuccessState() {
+        final ResourceHandlerRequest<ResourceModel> request = makeRequest(validResourceModel);
+
+        doReturn(DeleteProfilingGroupResponse.builder().build())
+                .when(proxy).injectCredentialsAndInvokeV2(any(), any());
+
         final ProgressEvent<ResourceModel, CallbackContext> response
-            = handler.handleRequest(proxy, request, null, logger);
+                = new DeleteHandler().handleRequest(proxy, request, null, logger);
 
         assertThat(response).isNotNull();
         assertThat(response.getStatus()).isEqualTo(OperationStatus.SUCCESS);
         assertThat(response.getCallbackContext()).isNull();
         assertThat(response.getCallbackDelaySeconds()).isEqualTo(0);
         assertThat(response.getResourceModel()).isEqualTo(request.getDesiredResourceState());
-        assertThat(response.getResourceModels()).isNull();
         assertThat(response.getMessage()).isNull();
         assertThat(response.getErrorCode()).isNull();
+    }
+
+    @Test
+    public void testNotFoundException() {
+        final ResourceHandlerRequest<ResourceModel> request = makeRequest(validResourceModel);
+
+        doThrow(ResourceNotFoundException.builder().build())
+                .when(proxy).injectCredentialsAndInvokeV2(any(), any());
+
+        assertThrows(CfnNotFoundException.class, () ->
+                new DeleteHandler().handleRequest(proxy, request, null, logger));
+    }
+
+    @Test
+    public void testInternalServerException() {
+        final ResourceHandlerRequest<ResourceModel> request = makeRequest(validResourceModel);
+
+        doThrow(InternalServerException.builder().build())
+                .when(proxy).injectCredentialsAndInvokeV2(any(), any());
+
+        assertThrows(CfnServiceInternalErrorException.class, () ->
+                new DeleteHandler().handleRequest(proxy, request, null, logger));
+    }
+
+    @Test
+    public void testThrottlingException() {
+        final ResourceHandlerRequest<ResourceModel> request = makeRequest(validResourceModel);
+
+        doThrow(ThrottlingException.builder().build())
+                .when(proxy).injectCredentialsAndInvokeV2(any(), any());
+
+        assertThrows(CfnThrottlingException.class, () ->
+                new DeleteHandler().handleRequest(proxy, request, null, logger));
+    }
+
+    @Test
+    public void testValidationException() {
+        final ResourceHandlerRequest<ResourceModel> request = makeRequest(invalidResourceModel);
+
+        doThrow(ValidationException.builder().build())
+                .when(proxy).injectCredentialsAndInvokeV2(any(), any());
+
+        assertThrows(CfnInvalidRequestException.class, () ->
+                new DeleteHandler().handleRequest(proxy, request, null, logger));
+    }
+
+    private ResourceHandlerRequest<ResourceModel> makeRequest(ResourceModel model) {
+        return ResourceHandlerRequest.<ResourceModel>builder()
+                .desiredResourceState(model)
+                .build();
     }
 }
